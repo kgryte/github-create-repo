74e8bd2a63a947d9f3a0451ab77939e58d376002
kgryte <kgryte@gmail.com>
2016-03-03 18:13:21 -0800

    Add support for additional options

--- test/test.validate.js
+++ test/test.validate.js
@@ -93,18 +93,323 @@ tape( 'if provided a `useragent` option which is not a primitive string, the fun
 	t.end();
 });
 
+tape( 'if provided a `org` option which is not a primitive string, the function returns a type error', function test( t ) {
+	var values;
+	var err;
+	var i;
+
+	values = [
+		5,
+		NaN,
+		null,
+		undefined,
+		true,
+		[],
+		{},
+		function(){}
+	];
+
+	for ( i = 0; i < values.length; i++ ) {
+		err = validate( {}, {
+			'token': 'abcdefg',
+			'org': values[i]
+		});
+		t.ok( err instanceof TypeError, 'returns type error when provided ' + values[i] );
+	}
+	t.end();
+});
+
+tape( 'if provided a `desc` option which is not a primitive string, the function returns a type error', function test( t ) {
+	var values;
+	var err;
+	var i;
+
+	values = [
+		5,
+		NaN,
+		null,
+		undefined,
+		true,
+		[],
+		{},
+		function(){}
+	];
+
+	for ( i = 0; i < values.length; i++ ) {
+		err = validate( {}, {
+			'token': 'abcdefg',
+			'desc': values[i]
+		});
+		t.ok( err instanceof TypeError, 'returns type error when provided ' + values[i] );
+	}
+	t.end();
+});
+
+tape( 'if provided a `homepage` option which is not a valid URI, the function returns a type error', function test( t ) {
+	var values;
+	var err;
+	var i;
+
+	values = [
+		'beep',
+		'be/ep/bo',
+		'//beep',
+		5,
+		NaN,
+		null,
+		undefined,
+		true,
+		[],
+		{},
+		function(){}
+	];
+
+	for ( i = 0; i < values.length; i++ ) {
+		err = validate( {}, {
+			'token': 'abcdefg',
+			'homepage': values[i]
+		});
+		t.ok( err instanceof TypeError, 'returns type error when provided ' + values[i] );
+	}
+	t.end();
+});
+
+tape( 'if provided a `team` option which is not a nonnegative integer, the function returns a type error', function test( t ) {
+	var values;
+	var err;
+	var i;
+
+	values = [
+		'beep',
+		-1,
+		5.123,
+		NaN,
+		null,
+		undefined,
+		true,
+		[],
+		{},
+		function(){}
+	];
+
+	for ( i = 0; i < values.length; i++ ) {
+		err = validate( {}, {
+			'token': 'abcdefg',
+			'team': values[i]
+		});
+		t.ok( err instanceof TypeError, 'returns type error when provided ' + values[i] );
+	}
+	t.end();
+});
+
+tape( 'if provided a `gitignore` option which is not a primitive string, the function returns a type error', function test( t ) {
+	var values;
+	var err;
+	var i;
+
+	values = [
+		5,
+		NaN,
+		null,
+		undefined,
+		true,
+		[],
+		{},
+		function(){}
+	];
+
+	for ( i = 0; i < values.length; i++ ) {
+		err = validate( {}, {
+			'token': 'abcdefg',
+			'gitignore': values[i]
+		});
+		t.ok( err instanceof TypeError, 'returns type error when provided ' + values[i] );
+	}
+	t.end();
+});
+
+tape( 'if provided a `license` option which is not a primitive string, the function returns a type error', function test( t ) {
+	var values;
+	var err;
+	var i;
+
+	values = [
+		5,
+		NaN,
+		null,
+		undefined,
+		true,
+		[],
+		{},
+		function(){}
+	];
+
+	for ( i = 0; i < values.length; i++ ) {
+		err = validate( {}, {
+			'token': 'abcdefg',
+			'license': values[i]
+		});
+		t.ok( err instanceof TypeError, 'returns type error when provided ' + values[i] );
+	}
+	t.end();
+});
+
+tape( 'if provided a `private` option which is not a primitive boolean, the function returns a type error', function test( t ) {
+	var values;
+	var err;
+	var i;
+
+	values = [
+		'beep',
+		5,
+		NaN,
+		null,
+		undefined,
+		[],
+		{},
+		function(){}
+	];
+
+	for ( i = 0; i < values.length; i++ ) {
+		err = validate( {}, {
+			'token': 'abcdefg',
+			'private': values[i]
+		});
+		t.ok( err instanceof TypeError, 'returns type error when provided ' + values[i] );
+	}
+	t.end();
+});
+
+tape( 'if provided a `issues` option which is not a primitive boolean, the function returns a type error', function test( t ) {
+	var values;
+	var err;
+	var i;
+
+	values = [
+		'beep',
+		5,
+		NaN,
+		null,
+		undefined,
+		[],
+		{},
+		function(){}
+	];
+
+	for ( i = 0; i < values.length; i++ ) {
+		err = validate( {}, {
+			'token': 'abcdefg',
+			'issues': values[i]
+		});
+		t.ok( err instanceof TypeError, 'returns type error when provided ' + values[i] );
+	}
+	t.end();
+});
+
+tape( 'if provided a `wiki` option which is not a primitive boolean, the function returns a type error', function test( t ) {
+	var values;
+	var err;
+	var i;
+
+	values = [
+		'beep',
+		5,
+		NaN,
+		null,
+		undefined,
+		[],
+		{},
+		function(){}
+	];
+
+	for ( i = 0; i < values.length; i++ ) {
+		err = validate( {}, {
+			'token': 'abcdefg',
+			'wiki': values[i]
+		});
+		t.ok( err instanceof TypeError, 'returns type error when provided ' + values[i] );
+	}
+	t.end();
+});
+
+tape( 'if provided a `downloads` option which is not a primitive boolean, the function returns a type error', function test( t ) {
+	var values;
+	var err;
+	var i;
+
+	values = [
+		'beep',
+		5,
+		NaN,
+		null,
+		undefined,
+		[],
+		{},
+		function(){}
+	];
+
+	for ( i = 0; i < values.length; i++ ) {
+		err = validate( {}, {
+			'token': 'abcdefg',
+			'downloads': values[i]
+		});
+		t.ok( err instanceof TypeError, 'returns type error when provided ' + values[i] );
+	}
+	t.end();
+});
+
+tape( 'if provided a `init` option which is not a primitive boolean, the function returns a type error', function test( t ) {
+	var values;
+	var err;
+	var i;
+
+	values = [
+		'beep',
+		5,
+		NaN,
+		null,
+		undefined,
+		[],
+		{},
+		function(){}
+	];
+
+	for ( i = 0; i < values.length; i++ ) {
+		err = validate( {}, {
+			'token': 'abcdefg',
+			'init': values[i]
+		});
+		t.ok( err instanceof TypeError, 'returns type error when provided ' + values[i] );
+	}
+	t.end();
+});
+
 tape( 'the function returns `null` if all options are valid', function test( t ) {
+	var options;
 	var opts;
 	var err;
 
-	opts = {};
-	err = validate( opts, {
+	options = {
 		'token': 'abcdefg',
-		'useragent': 'beeper-booper'
-	});
+		'useragent': 'beeper-booper',
+		'org': 'math-io',
+		'desc': 'beep',
+		'homepage': 'https://github.com/math-io',
+		'team': 1,
+		'gitignore': 'JavaScript',
+		'license': 'mit',
+		'private': false,
+		'issues': true,
+		'wiki': true,
+		'downloads': true,
+		'init': false
+	};
+
+	opts = {};
+	err = validate( opts, options );
+
 	t.equal( err, null, 'returns null' );
-	t.equal( opts.token, 'abcdefg', 'sets the token option' );
-	t.equal( opts.useragent, 'beeper-booper', 'sets the useragent option' );
+	t.deepEqual( opts, options, 'sets all options' );
 
 	t.end();
 });
